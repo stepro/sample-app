7f9bd02e126d512a9ad56419478431387f0077b9
Stephen Provine <stephpr@microsoft.com>
2016-11-01 12:33:36 -0700

    Update sample-app for preview.

--- service-a/server.js
+++ service-a/server.js
@@ -13,20 +13,29 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(morgan("dev"));
 
-// api ------------------------------------------------------------
-app.get('/api', function (req, res) {
-    // Invoke service-b
-    request('http://service-b', function (error, response, body) {
-         res.send('Hello from service A running on ' + os.hostname() + ' and ' + body);
-    });
-});
-
 // application -------------------------------------------------------------
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+// api ------------------------------------------------------------
+app.get('/api', function (req, res) {
+    // Invoke service-b
+    request('http://service-b', function (error, response, body) {
+        res.send('Hello from service A running on ' + os.hostname() + ' and ' + body);
+    });
+});
+
 var port = 80;
-app.listen(port, function () {
+var server = app.listen(port, function () {
     console.log('Listening on port ' + port);
+});
+
+process.on("SIGINT", () => {
+    process.exit(130 /* 128 + SIGINT */);
+});
+
+process.on("SIGTERM", () => {
+    console.log("Terminating...");
+    server.close();
 });
